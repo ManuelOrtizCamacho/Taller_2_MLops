87a113dd5fd2c63c57cb2c1109ec024a72bc724c
Sebas <sebastian.ovalle17@tau.usbmed.edu.co>
2024-11-26 20:44:55 -0500

    Cargue del proyecto

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from pandas import DataFrame
+import pandas as pd
 from joblib import load
 from pydantic import BaseModel, ValidationError
 from fastapi import FastAPI, HTTPException
-
+ 
 model = load("best_gbr_model.pkl")
-
+ 
 app = FastAPI()
-
+ 
 class JSON(BaseModel):
   Age: float
   Gender: float
@@ -21,18 +21,19 @@ class JSON(BaseModel):
   Music: float
   Volunteering: float
   GPA: float
-  
-@app.post("/predict")
-def predict(request: JSON):
-  try:
-    df_data = DataFrame(request)
-    prediction = model.predict(df_data)
-    return {"prediction": prediction.tolist()}
-  except ValidationError as ve:
-    raise HTTPException(status_code=400, detail=ve.errors())
-  except Exception as e:
-    raise HTTPException(status_code=500, detail=str(e))
 
+@app.post("/predict")
+def predict(request: list[JSON]):  # Cambiado para recibir una lista de objetos JSON
+    try:
+        data = [item.model_dump() for item in request]
+        df_data = pd.DataFrame(data)
+        predictions = model.predict(df_data)
+        return {"predictions": predictions.tolist() }
+    except ValidationError as ve:
+        raise HTTPException(status_code=400, detail=ve.errors())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+ 
 @app.get("/")
 def home():
   return {'Universidad EIA': 'Our Work'}
